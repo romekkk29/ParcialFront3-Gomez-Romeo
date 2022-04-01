a7a4d70bed34f04c3a8fed1d356459111a528d46
unknown <romegomez29@gmail.com>
2022-04-01 18:29:21 -0300

    mi game app

---
+++ app/src/components/History.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+class History extends React.Component{
+
+    render(){
+        return(
+            <div className="recordatorio">
+            <h3>Seleccion anterior: {this.props.seleccionPrevia} </h3>
+            <h4>Historial de opciones elegidas: </h4>
+                <ul>
+                  {
+                    this.props.historial.length>0 
+                    ?                            
+                      this.props.historial.map((element,index)=><li key={"selectionPrev"+index}>{element}</li>)
+                     : ""
+                  }
+                </ul>
+          </div>
+        )
+
+    }
+
+}
+
+export default History
